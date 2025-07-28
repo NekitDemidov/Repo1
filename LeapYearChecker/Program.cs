6cf27a15da364100c0e72bb658133fe9382fc68f
IPO0121027 Демидов Никита Олегович <IPO0121027@sibupk.su>
2025-07-28 11:53:59 +0700

    Добавлен улучшенный интерфейс

--- LeapYearChecker/Program.cs
+++ LeapYearChecker/Program.cs
@@ -4,12 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Проверка високосного года");
-            Console.Write("Введите год: ");
+            Console.WriteLine("=== Проверка високосного года ===");
+            Console.Write(">>> Введите год: ");
             int year = int.Parse(Console.ReadLine()!);
 
             bool isLeap = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
-            Console.WriteLine(isLeap ? $"{year} — високосный год." : $"{year} — не високосный год.");
+            Console.WriteLine($"Результат: {year} год {(isLeap ? "високосный" : "не високосный")}.");
+
         }
     }
 }
