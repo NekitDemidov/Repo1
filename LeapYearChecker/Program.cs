bbb8aa091adceb572108035310a0805db8e7b5e1
IPO0121027 Демидов Никита Олегович <IPO0121027@sibupk.su>
2025-07-28 11:42:45 +0700

    Создан проект: базовая проверка високосного года

---
+++ LeapYearChecker/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LeapYearChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка високосного года");
+            Console.Write("Введите год: ");
+            int year = int.Parse(Console.ReadLine()!);
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine($"{year} — високосный год.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} — не високосный год.");
+            }
+        }
+    }
+}
+
